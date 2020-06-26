f01e19b6efe6e21dc88d97f1ca8216c0ad8881e5
landmammal <evolpirate@gmail.com>
2020-06-26 10:35:39 -0500

    deleting todo items

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.set("view engine", "ejs");
 
 // MIDDLEWARE -------------------
 app.use(express.static("public")); // adding static assets (css, img, js files)
-app.use(express.urlencoded({extended: false})) // reads the data 
-app.use(express.json()) // format the data coming in as an object under a property call body
+app.use(express.urlencoded({ extended: false })); // reads the data
+app.use(express.json()); // format the data coming in as an object under a property call body
 
 var PORT = process.env.PORT || 3000;
 
@@ -18,34 +18,45 @@ var PORT = process.env.PORT || 3000;
 var todosItems = [
   { id: 1, text: "Get Milk" },
   { id: 2, text: "Work On Project" },
-  { id: 3, text: "Get my Nails done" }
+  { id: 3, text: "Get my Nails done" },
 ];
 
 // *********** ROUTES ***********
 
 // GET all todos
 app.get("/", function (req, res) {
-  res.render("list-of-todos.ejs", {list: todosItems });
+  res.render("list-of-todos.ejs", { list: todosItems });
 });
 
 // Get a single todo
 app.get("/todo/:id", function (req, res) {
-  var id = parseInt(req.params.id)
+  var id = parseInt(req.params.id);
   for (let i = 0; i < todosItems.length; i++) {
-    if (id === todosItems[i].id) {      
-      res.render("single-todo.ejs", {todo: todosItems[i]});
-    }    
+    if (id === todosItems[i].id) {
+      res.render("single-todo.ejs", { todo: todosItems[i] });
+    }
   }
 });
 
 // Post a todo
 app.post("/create-todo", function (req, res) {
   // console.log(req.body);
-  var todo = req.body
-  todo.id = Math.round(Math.random() * 2000)
+  var todo = req.body;
+  todo.id = Math.round(Math.random() * 2000);
 
-  todosItems.push(todo)
+  todosItems.push(todo);
   res.redirect("/");
+});
+
+// Destroying deleting todo
+app.get("/delete-todo/:id", function (req, res) {
+  var id = parseInt(req.params.id); // grab todo id from params
+  for (let i = 0; i < todosItems.length; i++) {
+    if (id === todosItems[i].id) {
+      todosItems.splice(i, 1);
+      res.redirect("/");
+    }
+  }
 });
 
 app.listen(PORT, function () {
