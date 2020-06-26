bcfc62d67e2738883e90e8bbc5a39dee82b2f187
landmammal <evolpirate@gmail.com>
2020-06-26 09:28:45 -0500

    adding database simulation

--- app.js
+++ app.js
@@ -12,8 +12,27 @@ app.use(express.static("public")); // adding static assets (css, img, js files)
 
 var PORT = process.env.PORT || 3000;
 
+// DATABASE SIMULATION
+var todosItems = [
+  { id: 1, text: "Get Milk" },
+  { id: 2, text: "Work On Project" },
+];
+
+// *********** ROUTES ***********
+
+// GET all todos
 app.get("/", function (req, res) {
   res.render("list-of-todos.ejs");
+});
+
+// Get a single todo
+app.get("/todo/:id", function (req, res) {
+  res.render("single-todo.ejs");
+});
+
+// Post a todo
+app.post("/create-todo", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(PORT, function () {
