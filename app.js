a079d6e0c5e74d041b0e261540d9f1b9822709c7
landmammal <evolpirate@gmail.com>
2020-06-26 10:04:28 -0500

    binding single item to singles todos page

--- app.js
+++ app.js
@@ -16,18 +16,24 @@ var PORT = process.env.PORT || 3000;
 var todosItems = [
   { id: 1, text: "Get Milk" },
   { id: 2, text: "Work On Project" },
+  { id: 3, text: "Get my Nails done" }
 ];
 
 // *********** ROUTES ***********
 
 // GET all todos
 app.get("/", function (req, res) {
-  res.render("list-of-todos.ejs");
+  res.render("list-of-todos.ejs", {list: todosItems });
 });
 
 // Get a single todo
 app.get("/todo/:id", function (req, res) {
-  res.render("single-todo.ejs");
+  var id = parseInt(req.params.id)
+  for (let i = 0; i < todosItems.length; i++) {
+    if (id === todosItems[i].id) {      
+      res.render("single-todo.ejs", {todo: todosItems[i]});
+    }    
+  }
 });
 
 // Post a todo
